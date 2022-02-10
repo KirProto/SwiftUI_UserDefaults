fe52efbda51c068ccb226b6253090b1cbbf650c6
Kir Proto Q <kir.proto@ya.ru>
2022-02-10 11:56:47 +0500

    Initial Commit

---
+++ Yan_SwiftUI_UserDefaults/Yan_SwiftUI_UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_UserDefaultsApp.swift
+//  Yan_SwiftUI_UserDefaults
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
